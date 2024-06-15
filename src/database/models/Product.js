d0cc3eee2c567b6bf0ada781ccebbe3c5514a037
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-06-15 15:51:45 -0300

    Modelo Product creado en la carpeta models

---
+++ src/database/models/Product.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} dataTypes
+ *
+ */
+//Product Model con Sequelize
+module.exports = (sequelize, dataTypes) => {
+  const alias = 'Product';
+  const cols = {
+    id: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: dataTypes.STRING(255),
+    },
+    description: {
+      type: dataTypes.STRING(255),
+    },
+    price: {
+      type: dataTypes.DECIMAL(11, 2),
+    },
+    product_categories_id: {
+      type: dataTypes.INTEGER,
+    }
+  }
+
+  const config = {
+    tableName: 'products',
+    timestamps: false,
+  }
+  const Product = sequelize.define(alias, cols, config);
+
+  return Product;
+}
