a80a5bc1c0523ca346e5a15108b0b4e367d60b44
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-06-16 20:50:54 -0300

    Controlador que crea un nuevo usuario

--- src/controllers/api/userController.js
+++ src/controllers/api/userController.js
@@ -12,6 +12,12 @@ const controller = {
     },
     async create (req, res) {
         // Controllador para crear un nuevo usuario
+        try {
+            const user = await db.User.create(req.body);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 }
 
