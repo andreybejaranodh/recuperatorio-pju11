7085858cb72da85d68bb4d5b12750663d1bd3c59
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-06-16 20:20:11 -0300

    Controlador que crea un nuevo role

--- src/controllers/api/roleController.js
+++ src/controllers/api/roleController.js
@@ -12,6 +12,20 @@ const controller = {
     },
     create: async (req, res) => {
         // Controllador para crear un nuevo usuario
+        try {
+            const { name } = req.body;
+
+            // Se verifica que se haya enviado el campo nombre
+            if (!name) {
+                // Si no se envió el campo nombre, se retorna un error
+                return res.status(400).json({ error: 'Falta el campo nombre' });
+            }
+            
+            const role = await db.Role.create({ name: name });
+            res.status(201).json(role);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 }
 
