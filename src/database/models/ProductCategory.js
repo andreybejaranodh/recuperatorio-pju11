bdba548878ec4715f43e4476f14cef0a60e42160
deotrolado <facundomatias642@gmail.com>
2024-06-16 12:24:43 -0300

    Se creo el modelo ProductCategory con los campos que estan en la tabla product_categories con otra estructura

---
+++ src/database/models/ProductCategory.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = 'ProductCategory';
+    const cols = {
+      id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: dataTypes.STRING(255),
+        allowNull: true, 
+      },
+    };
+    const config = {
+      tableName: 'product_categories',
+      timestamps: false, 
+    };
+    const ProductCategory = sequelize.define(alias, cols, config);
+  
+    return ProductCategory;
+  };
